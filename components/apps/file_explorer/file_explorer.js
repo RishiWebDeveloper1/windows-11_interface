a3cda98f0981621b6b7399ccd644dd3e1cd1fdaf
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-05-05 16:22:25 +0530

    Add files via upload

--- components/apps/file_explorer/file_explorer.js
+++ components/apps/file_explorer/file_explorer.js
@@ -1,29 +1,108 @@
-function handleDragOver(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'copy';
-}
+const imageInput = document.getElementById('imageInput');
+const uploadButton = document.getElementById('uploadButton');
+const showButton = document.getElementById('showButton');
+const imagePreview = document.getElementById('imagePreview');
 
-function handleFileDrop(event) {
-    event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    if (file && file.type.startsWith('image/')) {
-        const reader = new FileReader();
-        reader.onload = function (event) {
-            const imgPreview = document.getElementById('imagePreview');
-            imgPreview.src = event.target.result;
-            
+imageInput.addEventListener('change',
+
+    function () {
+        const file = imageInput.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function () {
+                const imageData = reader.result;
+                let imageName = file.name;
+                localStorage.setItem(imageName, imageData);
+                loadImageList();
+            };
         }
-        reader.readAsDataURL(file);
-    } else {
-        alert('Please drop an image file.');
+    }
+);
+
+function loadImageList() {
+    let allImage = document.querySelector('.loaded-file-box');
+    allImage.innerHTML = '';
+    const storage = window.localStorage;
+    const keys = Object.keys(storage);
+
+    for (const getImageName of keys) {
+        let imageCheck = ['.jpg', '.png', '.jpeg']
+
+        for (let i of imageCheck) {
+            let index = getImageName.indexOf(i);
+            
+            if (index !== -1) {
+                // console.log('in if')
+                
+                const folderDetailBox = document.createElement('div');
+                folderDetailBox.classList.add('folder-detail-box');
+                folderDetailBox.onclick = () => {
+                    showImage(getImageName);
+                }
+                
+                const folderDetailIconBox = document.createElement('div');
+                folderDetailIconBox.classList.add('folder-detail-icon-box');
+                
+                const folderIcon = document.createElement('img');
+                folderIcon.src = 'images/image_icon.png';
+                folderIcon.alt = 'image';
+                folderIcon.classList.add('folder-icon');
+                
+                const folderDetailContentBox = document.createElement('div');
+                folderDetailContentBox.classList.add('folder-detail-box');
+                
+                const folderDetailNameBox = document.createElement('div');
+                folderDetailNameBox.classList.add('folder-detail-name-box');
+                folderDetailNameBox.textContent = getImageName;
+                
+                const dateModifiedBox = document.createElement('div');
+                dateModifiedBox.classList.add('date-modified-box');
+                dateModifiedBox.textContent = '04-05-2024 06:08 PM';
+            
+                const typeBox = document.createElement('div');
+                typeBox.classList.add('type-box');
+                typeBox.textContent = 'File Folder';
+                
+                const sizeBox = document.createElement('div');
+                sizeBox.classList.add('size-box');
+                sizeBox.textContent = '10GB';
+                
+                folderDetailIconBox.appendChild(folderIcon);
+                folderDetailBox.appendChild(folderDetailIconBox);
+                folderDetailContentBox.appendChild(folderDetailNameBox);
+                folderDetailContentBox.appendChild(dateModifiedBox);
+                folderDetailContentBox.appendChild(typeBox);
+                folderDetailContentBox.appendChild(sizeBox);
+                folderDetailBox.appendChild(folderDetailContentBox);
+                allImage.appendChild(folderDetailBox);
+            }
+        }
     }
 }
 
-function showImage() {
-    const imgPreview = document.getElementById('imagePreview');
-    if (imgPreview.src != '#') {
-        alert('Image uploaded successfully!');
-    } else {
-        alert('Please upload an image first.');
-    }
+function showImage(imageName) {
+    let imagePreviewBox = document.querySelector('.image-preview-box');
+    let getImage = localStorage.getItem(imageName);
+    imagePreview.src = getImage;
+    imagePreviewBox.style.display = 'flex';
+    setTimeout(() => {
+        if (imagePreview.naturalWidth > imagePreview.naturalHeight) {
+            console.log('width is greater')
+            imagePreview.style.width = '100%';
+        }
+        else {
+            console.log('height is greater')
+            imagePreview.style.height = '100%';
+        }        
+    }, 500);
 }
+
+function closeImage() {
+    let imagePreviewBox = document.querySelector('.image-preview-box');
+    imagePreviewBox.style.display = 'none';
+    imagePreview.style.width = 'auto';
+    imagePreview.style.height = 'auto';
+}
+
+loadImageList();

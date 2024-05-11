7611f077a07f6c60b17b8703607ec9aca9f64d69
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-05-11 21:26:27 +0530

    Add files via upload

---
+++ components/application.js
@@ -0,0 +1,342 @@
+let noOfApplicatons = 0;
+
+function applicationLayoutManager() {
+    let application = document.getElementsByClassName('application');
+
+    if (noOfApplicatons == 1) {
+        for (let element of application) {
+            element.style.width = '50%';
+            element.style.height = '100%';
+            element.style.position = 'static';
+        }
+    }
+    else if (noOfApplicatons == 2) {
+        for (let element of application) {
+            element.style.width = '50%';
+            element.style.height = '100%';
+            element.style.position = 'static';
+        }
+    }
+    else if (noOfApplicatons == 3) {
+        for (let element of application) {
+            element.style.width = '33.3%';
+            element.style.height = '100%';
+            element.style.position = 'static';
+        }
+    }
+    else if (noOfApplicatons == 4) {
+        for (let element of application) {
+            element.style.width = '50%';
+            element.style.height = '50%';
+            element.style.position = 'static';
+        }
+    }
+    else if (noOfApplicatons == 5) {
+        for (let element of application) {
+            element.style.width = '33.3%';
+            element.style.height = '50%';
+            element.style.position = 'static';
+        }
+    }
+    else if (noOfApplicatons == 6) {
+        for (let element of application) {
+            element.style.width = '33.3%';
+            element.style.height = '50%';
+            element.style.position = 'static';
+        }
+    }
+}
+
+function nothingToDo() {
+    console.log('nothing for do');
+}
+
+function createApplication(text, url) {
+    if (noOfApplicatons >= 6) {
+        createAlertWindow();
+    }
+    else {
+        let randomId = Array.from({ length: 5 }, () => String.fromCharCode(97 + Math.floor(Math.random() * 26))).join('');
+
+        let applicatonBox = document.getElementById('applicationBox');
+        let applicaton = document.createElement('div');
+        applicaton.id = randomId;
+        let titleBar = document.createElement('div');
+        let appName = document.createElement('div');
+        // appName.onclick = function () {
+        //     maximizeWindow(randomId);
+        // }
+        let test = document.createElement('div');
+        // test.onclick = function () {
+        //     maximizeWindow(randomId);
+        // }
+        let operationBox = document.createElement('div');
+        let minimizeBox = document.createElement('div');
+        minimizeBox.onclick = function () {
+            minimizeWindow(randomId);
+        }
+        let minimizeIcon = document.createElement('img');
+        let maximizeBox = document.createElement('div')
+        maximizeBox.onclick = function () {
+            maximizeWindow(randomId);
+        }
+        maximizeBox.addEventListener('mouseenter', function () {
+            snapLayoutManager(randomId);
+        });
+        maximizeBox.addEventListener('mouseleave', function () {
+            snapLayoutManager(randomId);
+        });
+        let maximizeIcon = document.createElement('img')
+        let closeBox = document.createElement('div')
+        closeBox.onclick = function () {
+            deleteWindow(randomId);
+        }
+        let closeIcon = document.createElement('img')
+        let appPage = document.createElement('div')
+        let ifarme = document.createElement('iframe')
+
+        applicaton.classList.add('application');
+        titleBar.classList.add('title-bar');
+        appName.classList.add('app-name');
+        operationBox.classList.add('operation-box');
+        test.classList.add('test');
+        minimizeBox.classList.add('minimize-box');
+        minimizeIcon.classList.add('minimize-icon');
+        maximizeBox.classList.add('maximize-box');
+        maximizeIcon.classList.add('maximize-icon');
+        closeBox.classList.add('close-box');
+        closeIcon.classList.add('close-icon');
+        appPage.classList.add('app-page');
+        ifarme.classList.add('iframe');
+
+        minimizeIcon.src = 'images/minimize_icon.png';
+        maximizeIcon.src = 'images/maximize_icon.png';
+        closeIcon.src = 'https://static-00.iconduck.com/assets.00/cross-icon-2048x2048-vz9m0pj3.png';
+
+        noOfApplicatons = noOfApplicatons + 1;
+
+        minimizeBox.appendChild(minimizeIcon);
+        maximizeBox.appendChild(maximizeIcon);
+        closeBox.appendChild(closeIcon);
+        operationBox.appendChild(minimizeBox);
+        operationBox.appendChild(maximizeBox);
+        operationBox.appendChild(closeBox);
+        titleBar.appendChild(appName);
+        titleBar.appendChild(test);
+        titleBar.appendChild(operationBox);
+        appPage.appendChild(ifarme);
+        applicaton.appendChild(titleBar);
+        applicaton.appendChild(appPage);
+        applicatonBox.appendChild(applicaton);
+
+        appName.textContent = text;
+        ifarme.src = url;
+        ifarme.style.border = '0';
+
+        applicationLayoutManager();
+    }
+}
+
+function createAlertWindow() {
+    let alertWindowBox = document.createElement('div');
+    let alertmessage = document.createElement('div');
+    let alertbutton = document.createElement('div');
+    alertbutton.addEventListener('click', deleteAlertWindow);
+
+    alertWindowBox.id = "alertWindowBox";
+    alertWindowBox.classList.add('alert-window-box');
+    alertmessage.classList.add('alert-messag');
+    alertbutton.classList.add('alert-button');
+
+    alertmessage.textContent = "Can't open more then 6 application";
+    alertbutton.textContent = "OK";
+
+    alertWindowBox.appendChild(alertmessage);
+    alertWindowBox.appendChild(alertbutton);
+
+    let applicatonBox = document.getElementById('applicationBox');
+    applicatonBox.appendChild(alertWindowBox);
+}
+
+function deleteAlertWindow() {
+    var windowFrame = document.getElementById("alertWindowBox");
+    console.log(windowFrame);
+    windowFrame.remove();
+}
+
+function minimizeWindow(idName) {
+    let appFullScreen = document.getElementById(idName)
+
+    appFullScreen.style.position = 'static';
+    appFullScreen.style.width = '50%';
+    appFullScreen.style.height = '100%';
+
+    applicationLayoutManager();
+}
+
+function maximizeWindow(idName) {
+    let appFullScreen = document.getElementById(idName);
+
+    appFullScreen.style.position = 'fixed';
+    appFullScreen.style.width = '100%';
+    appFullScreen.style.height = '100%';
+}
+
+function deleteWindow(idName) {
+    var windowFrame = document.getElementById(idName);
+    windowFrame.remove();
+
+    noOfApplicatons = noOfApplicatons - 1;
+
+    applicationLayoutManager();
+}
+
+let test2 = true;
+function snapLayoutManager(idName) {
+    // console.log('maximize movse hover');
+    if (document.querySelector('.snap-layout-box')) {
+        let snapLayoutManager = document.querySelector('.snap-layout-box');
+        snapLayoutManager.addEventListener('mouseenter', () => {
+            console.log('snap layout box entery of mouse');
+            return;
+        })
+    }
+
+    const firstParagraph = document.querySelector(`#${idName} .title-bar .operation-box`);
+
+    if (test2 == true) {
+        let snapLayoutBox = document.createElement('div');
+        snapLayoutBox.classList.add('snap-layout-box');
+
+        let boxesData = [
+            ['box1', 'box2'],
+            ['box3', 'box4'],
+            ['box5', 'box6', 'box7'],
+            ['box8', 'box9', 'box10', 'box11']
+        ];
+
+        boxesData.forEach(boxData => {
+            let boxContainer = document.createElement('div');
+            boxContainer.classList.add('box-container');
+            let boxContainer3 = document.createElement('div');
+            boxContainer3.classList.add('box-container3');
+            let lastBoxName = '';
+
+            boxData.forEach(boxName => {
+                if (boxName == "box8" || boxName == "box9" || boxName == "box10" || boxName == "box11") {
+                    console.log('box8 to box11');
+                    let box = document.createElement('div');
+                    box.classList.add(boxName);
+                    box.onclick = function () {
+                        snapLayoutMiniManager(idName, box);
+                    }
+                    boxContainer3.appendChild(box);
+                    lastBoxName = boxName;
+                }
+                else {
+                    let box = document.createElement('div');
+                    box.classList.add(boxName);
+                    box.onclick = function () {
+                        snapLayoutMiniManager(idName, box);
+                    }
+                    boxContainer.appendChild(box);
+                }
+            });
+            if (lastBoxName == 'box11')  {
+                snapLayoutBox.appendChild(boxContainer3);
+            }
+            else {
+                snapLayoutBox.appendChild(boxContainer);
+            }
+        });
+
+        firstParagraph.appendChild(snapLayoutBox);
+        test2 = false;
+    }
+    else {
+        if (document.querySelector('.snap-layout-box')) {
+            document.querySelector('.snap-layout-box').remove();
+            test2 = true;
+        }
+    }
+}
+
+function snapLayoutMiniManager(idName, box) {
+    if (box == 'box1') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box2') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box3') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box4') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box5') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box6') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box7') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box8') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box9') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box10') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+    else if (box == 'box11') {
+        let application = document.getElementById(idName);
+
+        application.style.width = '50%';
+        application.style.height = '100%';
+        application.style.position = 'fixed';
+    }
+}
